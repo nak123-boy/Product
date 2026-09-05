6ef47c57311d76351e81933e1be5fd976a050e26
nak123-boy <bongnak5217@gmail.com>
2026-09-05 11:48:27 +0700

    Prepare React app for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/Product/",
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
   server: {
     port: 5173,
+
     proxy: {
-      "/api": "http://127.0.0.1:8000"
-    }
-  }
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
